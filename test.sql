b2f59c283dc9a158e4abf63fb6fb4282465d3572
giomen88 <giorgia9288@virgilio.it>
2022-09-06 17:58:57 +0200

    select n.5

--- test.sql
+++ test.sql
@@ -11,10 +11,10 @@ SELECT * FROM `courses` WHERE `cfu` > 9;
 SELECT * FROM `students` WHERE YEAR(`date_of_birth`) > 1992;
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 
-
+SELECT * FROM `courses` WHERE `period` LIKE 'I semestre' AND `year` LIKE '1';
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 
-
+SELECT * FROM `exams` WHERE `date` = '2020-06-20' AND HOUR(`hour`) > 13;
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
 
 

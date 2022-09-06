9c1d5f5325a32b890af63fb391cf3c629dab1dca
giomen88 <giorgia9288@virgilio.it>
2022-09-06 18:38:38 +0200

    group by n.1

--- test.sql
+++ test.sql
@@ -30,7 +30,7 @@ SELECT * FROM `teachers` WHERE `phone` IS NULL;
 
 -- 1. Contare quanti iscritti ci sono stati ogni anno
 
-
+SELECT COUNT(id) AS `total_students`, YEAR(`enrolment_date`) FROM `students` GROUP BY YEAR(`enrolment_date`);
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 
 

43bc9ec87974bfe28503d889bd80b9fab5428242
giomen88 <giorgia9288@virgilio.it>
2022-09-07 17:58:14 +0200

    n.3

--- join.sql
+++ join.sql
@@ -1,10 +1,25 @@
 -- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
-SELECT * FROM `students` JOIN `degrees` ON `degrees`.`id` = `students`. `degree_id` WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
+SELECT * 
+FROM `students` 
+JOIN `degrees` 
+ON `degrees`.`id` = `students`. `degree_id` 
+WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
 
 -- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
-SELECT * FROM `degrees` JOIN `departments` ON `departments`.`id` = `degrees`.`department_id` WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
+SELECT * 
+FROM `degrees` 
+JOIN `departments` 
+ON `departments`.`id` = `degrees`.`department_id` 
+WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+SELECT * 
+FROM `courses` 
+JOIN `course_teacher` 
+ON `courses`.`id` = `course_teacher`.`course_id` 
+JOIN `teachers` 
+ON `teachers`.`id` = `course_teacher`.`teacher_id` 
+WHERE `teachers`.`id` = 44;
 
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il
 -- relativo dipartimento, in ordine alfabetico per cognome e nome

3917e2d3939e4095213ec40ff1692c7333e176bc
giomen88 <giorgia9288@virgilio.it>
2022-09-07 18:10:30 +0200

    n.4

--- join.sql
+++ join.sql
@@ -23,6 +23,13 @@ WHERE `teachers`.`id` = 44;
 
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il
 -- relativo dipartimento, in ordine alfabetico per cognome e nome
+SELECT `students`.`surname` AS `Cognome`, `students`.`name` AS `Nome`, `degrees`.`name` AS `Corso di Laurea`, `departments`.`name` AS `Facoltà`
+FROM `students`
+JOIN `degrees` 
+ON `degrees`.`id` = `students`. `degree_id` 
+JOIN `departments` 
+ON `departments`.`id` = `degrees`. `department_id`
+ORDER BY `students`.`surname` ASC;
 
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 

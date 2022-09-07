3fd79e777f7248e4b0d39b583c47405e5973b7cb
giomen88 <giorgia9288@virgilio.it>
2022-09-07 17:46:50 +0200

    n.1

--- join.sql
+++ join.sql
@@ -1,4 +1,5 @@
 -- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+SELECT * FROM `students` JOIN `degrees` ON `degrees`.`id` = `students`. `degree_id` WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
 
 -- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
 
